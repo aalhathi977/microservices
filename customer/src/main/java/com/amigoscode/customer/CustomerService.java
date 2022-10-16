9b852e35cbac7270cef4a85086294433ce080605
aalharthi <a.alharthi977@gmail.com>
2022-10-16 15:50:49 +0300

    Added Notification service

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,11 +2,13 @@ package com.amigoscode.customer;
 
 import com.amigoscode.clients.fraud.FraudCheckResponse;
 import com.amigoscode.clients.fraud.FraudClient;
+import com.amigoscode.clients.notification.NotificationClient;
+import com.amigoscode.clients.notification.NotificationRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 @Slf4j
 @Service
-public record CustomerService(CustomerRepository customerRepository, CustomerConfig restTemplate, FraudClient fraudClient) {
+public record CustomerService(CustomerRepository customerRepository, CustomerConfig restTemplate, FraudClient fraudClient, NotificationClient notificationClient) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -18,6 +20,14 @@ public record CustomerService(CustomerRepository customerRepository, CustomerCon
         if (fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("fraudster");
         }
+        notificationClient.sendNotification(
+                new NotificationRequest(
+                        customer.getId(),
+                        customer.getEmail(),
+                        String.format("Hi %s, welcome to Amigoscode...",
+                                customer.getFirstName())
+                )
+        );
         log.info("customer request is processed");
     }
 }
